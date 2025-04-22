4c23c7d93973e04714dbedf6b9c908a66678e5f9
drewandrewd <andrewid10@gmail.com>
2025-04-22 22:34:11 +0500

    Added test for controller, service and mapper

---
+++ src/test/java/com/example/schoolproject/integration/PostgresContainer.java
@@ -0,0 +1,21 @@
+package com.example.schoolproject.integration;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class PostgresContainer {
+
+    @Container
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
+    @DynamicPropertySource
+    public static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
